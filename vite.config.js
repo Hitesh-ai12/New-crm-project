7b3e8d2c802d8f71b22d5f976ac1045fb6dc7227
Hitesh-ai12 <khusidaring777@gmail.com>
2025-05-28 16:22:20 +0530

    upadeted functionlites

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import laravel from 'laravel-vite-plugin'
-import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import laravel from 'laravel-vite-plugin'
+import { fileURLToPath } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [vue({
     template: {
       transformAssetUrls: {
-        base: null,
+        base: '/',
         includeAbsolute: false,
       },
     },
