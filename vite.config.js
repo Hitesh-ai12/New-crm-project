726b05f43abaa98279a3537869c95148f1df10f5
Hitesh-ai12 <khusidaring777@gmail.com>
2025-06-18 06:30:10 +0530

    changes add old vite

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,8 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/',
+
   plugins: [vue({
     template: {
       transformAssetUrls: {
