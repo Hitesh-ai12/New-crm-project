95bd4e494c0ddefc20cf07a618b5f62fe6482743
Hitesh-ai12 <khusidaring777@gmail.com>
2025-07-11 11:55:47 +0530

    and echo.js file

--- resources/js/plugins/echo.js
+++ resources/js/plugins/echo.js
@@ -1,5 +1,4 @@
-// resources/js/plugins/echo.js
-
+// ✅ Correct
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
@@ -7,13 +6,9 @@ window.Pusher = Pusher;
 
 const echo = new Echo({
   broadcaster: 'pusher',
-  key: import.meta.env.VITE_PUSHER_APP_KEY, // Prefer VITE_ variables
-  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'ap2',
-  wsHost: window.location.hostname,
-  wsPort: 6001,
-  forceTLS: false,
-  disableStats: true,
-  enabledTransports: ['ws', 'wss'],
+  key: import.meta.env.VITE_PUSHER_APP_KEY,
+  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+  forceTLS: true,
 });
 
 export default echo;
